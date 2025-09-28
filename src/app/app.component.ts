ae01f657f275aac1561744b3859813c2e500dc9e
Javier Hernandez <cruz.hernandez1@catolica.edu.sv>
2025-09-28 02:15:05 -0600

    feat: se acregaron conponentes globales de navar y sliderbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavarComponent } from "./navar-component/navar/navbar.component";
+import { SidebarComponent } from "./navar-component/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavarComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
